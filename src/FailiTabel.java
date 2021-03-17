33499ab0b1660b80273a8abf8ebdc55e0a8c16c8
mariokaara <58540861+mariokaara@users.noreply.github.com>
2021-03-17 19:19:31 +0200

    Muudatused
    Lõin liidese Tabel ja tema kaks implementatisooni Mälutabel ja FailiTabel. FailiTabeli klassi tuleks nüüd teha kõik faili ja failist lugemise võimalused.

---
+++ src/FailiTabel.java
@@ -0,0 +1,17 @@
+public class FailiTabel implements Tabel{
+
+    @Override
+    public void lisaAuto(Auto auto) {
+
+    }
+
+    @Override
+    public void kustutaAuto(int autoId) {
+
+    }
+
+    @Override
+    public String prindiTabel() {
+        return null;
+    }
+}
--- src/Tabel.java
+++ src/Tabel.java
@@ -1,31 +1,5 @@
-import java.util.ArrayList;
-
-public class Tabel {
-    ArrayList<Auto> autoTabel = new ArrayList<>();
-
-    public void lisaAuto(Auto auto) {
-        autoTabel.add(auto);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Kuupäev    |       VIN       |   Mark   |    Mudel    |    Asukoht    |    Klient    |\n");
-        for (Auto auto: autoTabel) {
-            sb.append(auto.getKuupäev());
-            sb.append("|");
-            sb.append(auto.getVin());
-            sb.append("|");
-            sb.append(auto.getMark());
-            sb.append("|");
-            sb.append(auto.getMudel());
-            sb.append("|");
-            sb.append(auto.getAsukoht());
-            sb.append("|");
-            sb.append(auto.getKlient());
-            sb.append("|");
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
+public interface Tabel {
+    void lisaAuto(Auto auto);
+    void kustutaAuto(int autoId);
+    String prindiTabel();
 }
